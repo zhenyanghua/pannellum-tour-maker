b3c7e0c1afbd0227721c4c389a397d26fa293e7f
Zhenyang Hua <zhenyang.hua@woolpert.com>
2017-12-12 11:31:11 -0500

    wip - Add UI to delete Tour

--- src/main/resources/static/js/tours.js
+++ src/main/resources/static/js/tours.js
@@ -1,6 +1,21 @@
 var $tourList = $("#tour-list-container");
+var $modalDeleteTour = $("#modal-delete-tour");
+var $deleteTourYes = $("#delete-tour-yes");
+
+var selectedTourName;
 
 getTours();
+
+function onDeleteTour(tourName) {
+	$modalDeleteTour.find('code').text(tourName);
+	$modalDeleteTour.modal("open");
+	selectedTourName = tourName;
+}
+
+function doDeleteTour() {
+	if (!selectedTourName) return;
+	console.log('delete tour ' + selectedTourName);
+}
 
 function getTours() {
 	$.getJSON(apiUrl + "/public/guest/tours/")
@@ -16,9 +31,20 @@ function getTours() {
 						.append($("<div>").addClass("card-content")
 							.append($("<span>").addClass("card-title red-text text-lighten-2").text(tour.name)))
 						.append($("<div>").addClass("card-action")
-							.append($("<a>").attr("href", "/tours/" + tour.name)
-								.text("Edit Tour")
-							))));
+							.append($("<a>").addClass('btn-floating blue tooltipped').attr("href", "/tours/" + tour.name)
+								.attr('data-position', 'top').attr('data-delay', 50).attr('data-tooltip', 'Edit Tour')
+								.append($("<i>").addClass('material-icons').text("edit")))
+							.append($("<a>").addClass('btn-floating red tooltipped').attr("href", "#")
+								.attr('data-position', 'top').attr('data-delay', 50).attr('data-tooltip', 'Delete Tour')
+								.append($("<i>").addClass('material-icons').text("delete"))
+								.click(function() {onDeleteTour(tour.name);}))
+						)));
 			});
+
+			$('.tooltipped').tooltip();
+			$(".modal").modal({
+				dismissible: false
+			});
+			$deleteTourYes.click(doDeleteTour);
 		});
 }
