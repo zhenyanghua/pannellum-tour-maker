29b0f4810c87659234d987c4d2babfbef377620f
Zhenyang Hua <zhenyang.hua@woolpert.com>
2018-01-25 15:44:29 -0500

    update version, add run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+docker run -it -e "SPRING_DATA_MONGODB_URI=mongodb://172.17.0.3:27017/panorama" \
+               -e "SPRING_RABBITMQ_HOST=172.17.0.4" \
+               -e "SPRING_RABBITMQ_PORT=5672" \
+               -e "SPRING_RABBITMQ_USERNAME=guest" \
+               -e "SPRING_RABBITMQ_PASSWORD=guest" \
+               -e "APPLICATION_BASEURL=http://tour-editor.leafyjava.com" \
+    -v /data:/home/pannellum-tour-maker \
+    --name tour-editor \
+    -p 80:80 \
+    -d \
+    downhillski/pannellum-tour-maker
