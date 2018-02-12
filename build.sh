128d83c9a860fd597e489a0110c5d3a65a9af088
Zhenyang Hua <zhenyang.hua@woolpert.com>
2018-02-12 15:01:57 -0500

    update aws ecr build script

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-export DOCKER_REGISTRY=localhost:5000
-export IMAGE_NAME=pannellum-tour-maker
+#export DOCKER_REGISTRY=localhost:5000
+#export IMAGE_NAME=pannellum-tour-maker
+
+export DOCKER_REGISTRY=227139575913.dkr.ecr.us-east-1.amazonaws.com
+export IMAGE_NAME=client_entergy_pannellum-tour-maker
 
 mvn clean package
 docker build -t ${DOCKER_REGISTRY}/${IMAGE_NAME} .
